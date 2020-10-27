7a96a542b7dddb3c4cbdcf0938bc13130bed83c3
amand4nunes <amandanunes.contato@gmail.com>
2020-10-27 15:46:24 -0300

    validação de campos vazios

--- app/src/main/java/com/example/vamos_lucrar/views/CadastroActivity.kt
+++ app/src/main/java/com/example/vamos_lucrar/views/CadastroActivity.kt
@@ -23,37 +23,70 @@ class CadastroActivity : AppCompatActivity() {
     }
 
 
-    fun novoContato( v: View ){
+    fun novoContato(v: View) {
+        var validacaoInput = 4
+        val nomeValid = nome_et.text.toString()
+        val emailValid = email_et.text.toString()
+        val senhaValid = senha_et.text.toString()
+        val telefoneValid = telefone_et.text.toString()
 
-    val novoContato = User(
-          null,
-          nome_et.text.toString(),
-          email_et.text.toString(),
-          senha_et.text.toString(),
-          telefone_et.text.toString(),
-           null
+        if (nomeValid.isEmpty()) {
+            nome_et.error = "Nome obrigatorio"
+            nome_et.requestFocus()
+            validacaoInput--
+        }
+        if (emailValid.isEmpty()) {
+            email_et.error = "Email obrigatorio"
+            email_et.requestFocus()
+            validacaoInput--
+        }
+        if (senhaValid.isEmpty()) {
+            senha_et.error = "Senha obrigatorio"
+            senha_et.requestFocus()
+            validacaoInput--
+        }
+        if (telefoneValid.isEmpty()) {
+            telefone_et.error = "Telefone é obrigatorio"
+            telefone_et.requestFocus()
+            validacaoInput--
+        }
 
-      )
+        if (validacaoInput == 4) {
 
 
+            val novoContato = User(
+                null,
+                nome_et.text.toString(),
+                email_et.text.toString(),
+                senha_et.text.toString(),
+                telefone_et.text.toString(),
+                null
 
-      val callNovoContato = remote.postContato(novoContato)
+            )
 
-      callNovoContato.enqueue(object: Callback<Void> {
-          override fun onFailure(call: Call<Void>, t: Throwable) {
-              Toast.makeText(baseContext, "Erro: $t", Toast.LENGTH_SHORT).show()
-          }
 
-          override fun onResponse(call: Call<Void>, response: Response<Void>) {
-              Toast.makeText(baseContext,
-                  getString(R.string.sucesso_cad),
-                  Toast.LENGTH_SHORT).show()
-          }
-      })}
+            val callNovoContato = remote.postContato(novoContato)
 
-    fun listaDeContato(v: View) {
-        val contatos = Intent(applicationContext, ListaContatosActivity::class.java)
-        startActivity(contatos)
-    }
+            callNovoContato.enqueue(object : Callback<Void> {
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    Toast.makeText(baseContext, "Erro: $t", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.sucesso_cad),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+        } else{
+            Toast.makeText(this, "Todos os campos são obrigatórios", Toast.LENGTH_SHORT).show()
+        }}
+
+        fun listaDeContato(v: View) {
+            val contatos = Intent(applicationContext, ListaContatosActivity::class.java)
+            startActivity(contatos)
+        }
 
 }
